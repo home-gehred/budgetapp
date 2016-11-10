cdaa2ce5a40d6df0162d375f6acab42b210ba78e
Robert Gehred <bagehred@sbcglobal.net>
2016-11-10 08:36:53 -0800

    clean me up

--- src/js/components/expense/ExpenseItem.js
+++ src/js/components/expense/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ListGroupItem } from 'react-bootstrap';
+import * as Accounting from "../../accounting.js";
 
 export default class ExpenseItem extends React.Component {
   constructor() {
@@ -7,13 +8,13 @@ export default class ExpenseItem extends React.Component {
   }
 
   render() {
-    console.log("ExpenseItem Props: ", this.props);
+    var formatedAmount = Accounting.formatMoney(this.props.amount);
     var classForItem = (this.props.include) ? "list-group-item list-group-item-success" : "list-group-item";
     return (
       <div>
         <a href="#" class={classForItem}>
           <h4 class="list-group-item-heading">{this.props.name}</h4>
-          <p class="list-group-item-text">${this.props.amount}</p>
+          <p class="list-group-item-text">{formatedAmount}</p>
           <p class="list-group-item-text"><b>Due Date:</b> <i>{this.props.duedate}</i></p>
         </a>
       </div>
